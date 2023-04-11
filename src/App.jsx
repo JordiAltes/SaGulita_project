9f0b05adfc9cdd5a57ceb0c87496afa31fc4d4e8
JordiAltes <jordalan8@gmail.com>
2023-04-11 12:11:03 +0200

    seguim amb pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "../src/index.css"
+import "../src/index.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Header />
       <Home />
-      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Mapa from "../components/MapSaGulita";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
     <main>
+      <Header />
       <div className="saGulita">
         <div className="saGulitaImage"></div>
         <div className="saGulitaText">
@@ -36,9 +43,9 @@ function Home() {
         </div>
         <div className="foodImage"></div>
       </div>
-      <div className="saGulita">
+      <div className="saGulita2">
         <div className="mapaImage">
-            <Mapa />
+          <Mapa />
         </div>
         <div className="mapaText">
           <h2>Donde estamos?</h2>
@@ -54,6 +61,27 @@ function Home() {
           </p>
         </div>
       </div>
+      <div className="saGulita2">
+        <div className="calendarText">
+            <h2>Horario y contacto</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            officiis quae sequi vero, dolor consequuntur porro doloremque
+            doloribus earum voluptas explicabo voluptatem excepturi. Quaerat
+            ipsa possimus, hic commodi unde aut! Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Ullam, officiis itaque! Deleniti
+            consequatur vero nisi labore, ab molestiae enim quidem perferendis
+            libero earum error, tempora doloremque officiis illo numquam
+            tempore.
+          </p>
+        </div>
+        <div className="calendarSymbol">
+          <FontAwesomeIcon icon={faCalendar} />
+          <FontAwesomeIcon icon={faClock} />
+          <FontAwesomeIcon icon={faPhone} />
+        </div>
+      </div>
+      <Footer />
     </main>
   );
 }
